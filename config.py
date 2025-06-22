eba0ce9ca8f34e9e99d994d45e828859814d52fb
Polina Zavyalova <zzavyaloffa@gmail.com>
2025-06-22 22:22:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 class Config:
     TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+    ADMINS = os.getenv('ADMINS')
     if not TOKEN:
         raise ValueError("Не найден TELEGRAM_BOT_TOKEN в .env файле!")
